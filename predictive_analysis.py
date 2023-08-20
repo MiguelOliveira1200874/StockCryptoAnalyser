0e65d4faccfc581e21287a873008b152ad4a5c5c
Miguel Oliveira <1200874@isep.ipp.pt>
2023-08-20 16:57:08 +0100

    aider: Fill in the `reset` and `render` methods in the `TradingEnv` class in `predictive_analysis.py`.
    # Aider chat conversation:
    
    USER: So what is missing now?
    
    ASSISTANT: From the provided code, it seems that the `step` method in the `TradingEnv` class in the `predictive_analysis.py` file is not yet implemented. This method is crucial for the reinforcement learning algorithm as it defines how the agent interacts with the environment.
    
    The `step` method should:
    1. Execute the action (buy, sell, or hold) in the trading environment.
    2. Calculate the reward based on the action and the current and new prices.
    3. Update the current step and done flag.
    4. Return the new observation, reward, done flag, and additional info.
    
    Also, the `_get_observation` method mentioned in the `reset` method is not defined. This method should return the current observation of the environment.
    
    Finally, the `n` in the `self.observation_space` is not defined. This should be the number of features in the observation.
    
    Here are the changes:
    
    ```python
    predictive_analysis.py
    <<<<<<< HEAD
        def step(self, action):
            # Execute one time step within the environment
            pass
    =======
        def step(self, action):
            # Execute one time step within the environment
            self.current_step += 1
    
            # Execute the action and update the balance and shares
            # ...
    
            # Calculate the reward
            reward = self.balance - self.initial_balance
            self.total_reward += reward
    
            # Check if we're done
            if self.current_step >= len(self.df):
                self.done = True
    
            return self._get_observation(), reward, self.done, {}
    >>>>>>> updated
    ```
    
    ```python
    predictive_analysis.py
    <<<<<<< HEAD
        def reset(self):
            # Reset the state of the environment to an initial state
            self.current_step = 0
            self.total_reward = 0.0
            self.done = False
            return self._get_observation()
    =======
        def reset(self):
            # Reset the state of the environment to an initial state
            self.current_step = 0
            self.total_reward = 0.0
            self.done = False
            self.balance = self.initial_balance
            self.shares = 0
            return self._get_observation()
    
        def _get_observation(self):
            # Return the current observation
            return self.df.loc[self.current_step]
    >>>>>>> updated
    ```
    
    ```python
    predictive_analysis.py
    <<<<<<< HEAD
        def __init__(self, df):
            super(TradingEnv, self).__init__()
    
            # Define action and observation space
            # They must be gym.spaces objects
            # Example when using discrete actions, Box for continuous action
            self.action_space = spaces.Discrete(3)
            self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(n,), dtype=np.float32)
    
            # Load data from a pandas dataframe
            self.df = df
            self.reward_range = (-np.inf, np.inf)
    =======
        def __init__(self, df, initial_balance=10000):
            super(TradingEnv, self).__init__()
    
            # Define action and observation space
            # They must be gym.spaces objects
            # Example when using discrete actions, Box for continuous action
            self.action_space = spaces.Discrete(3)
            self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(df.shape[1],), dtype=np.float32)
    
            # Load data from a pandas dataframe
            self.df = df
            self.reward_range = (-np.inf, np.inf)
    
            # Initialize additional attributes
            self.initial_balance = initial_balance
            self.balance = self.initial_balance
            self.shares = 0
    >>>>>>> updated
    ```
    
    Please note that the `step` method is simplified and you need to implement the logic for executing the action and updating the balance and shares.

--- predictive_analysis.py
+++ predictive_analysis.py
@@ -8,29 +8,53 @@ class TradingEnv(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, df):
+    def __init__(self, df, initial_balance=10000):
         super(TradingEnv, self).__init__()
 
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions, Box for continuous action
         self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(n,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(df.shape[1],), dtype=np.float32)
 
         # Load data from a pandas dataframe
         self.df = df
         self.reward_range = (-np.inf, np.inf)
 
+        # Initialize additional attributes
+        self.initial_balance = initial_balance
+        self.balance = self.initial_balance
+        self.shares = 0
+
     def step(self, action):
         # Execute one time step within the environment
-        pass
+        self.current_step += 1
+
+        # Execute the action and update the balance and shares
+        # ...
+
+        # Calculate the reward
+        reward = self.balance - self.initial_balance
+        self.total_reward += reward
+
+        # Check if we're done
+        if self.current_step >= len(self.df):
+            self.done = True
+
+        return self._get_observation(), reward, self.done, {}
 
     def reset(self):
         # Reset the state of the environment to an initial state
         self.current_step = 0
         self.total_reward = 0.0
         self.done = False
+        self.balance = self.initial_balance
+        self.shares = 0
         return self._get_observation()
+
+    def _get_observation(self):
+        # Return the current observation
+        return self.df.loc[self.current_step]
 
     def render(self, mode='human', close=False):
         # Render the environment to the screen
