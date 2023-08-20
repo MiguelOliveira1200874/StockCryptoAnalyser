a696e94554fce2572f7b1d0e5f901507505ab51f
Miguel Oliveira <1200874@isep.ipp.pt>
2023-08-20 17:13:09 +0100

    aider: Added functions to calculate moving averages and support/resistance levels in `technical_analysis.py`.
    # Aider chat conversation:
    
    USER: Agora comeca a trabalhar na `technical_analysis.py`, quero que uses trends e levels
    
    ASSISTANT: To implement trend and level analysis, we will need to add functions to calculate these in the `technical_analysis.py` file.
    
    Trends can be calculated using moving averages, and levels can be calculated using support and resistance levels.
    
    Here are the steps we will follow:
    
    1. Add a function to calculate moving averages, which will help us determine the trend.
    2. Add a function to calculate support and resistance levels, which will help us determine the levels.
    
    Here are the changes we need to make:
    
    ```python
    technical_analysis.py
    <<<<<<< HEAD
    # Functions for technical analysis
    def analyze_data():
        pass
    =======
    # Functions for technical analysis
    import numpy as np
    
    def calculate_moving_average(data, window_size):
        return data['4a. close (USD)'].rolling(window=window_size).mean()
    
    def calculate_support_resistance(data, window_size):
        support = data['4a. close (USD)'].rolling(window=window_size).min()
        resistance = data['4a. close (USD)'].rolling(window=window_size).max()
        return support, resistance
    
    def analyze_data(data):
        # Calculate the moving average with a window size of 20
        data['Moving Average'] = calculate_moving_average(data, 20)
    
        # Calculate the support and resistance levels with a window size of 20
        data['Support'], data['Resistance'] = calculate_support_resistance(data, 20)
    
        return data
    >>>>>>> updated
    ```
    
    Please note that the window size for the moving average and support/resistance calculations is set to 20. This is a common choice, but you may want to adjust it based on your specific needs.

--- technical_analysis.py
+++ technical_analysis.py
@@ -1,3 +1,19 @@
 # Functions for technical analysis
-def analyze_data():
-    pass
+import numpy as np
+
+def calculate_moving_average(data, window_size):
+    return data['4a. close (USD)'].rolling(window=window_size).mean()
+
+def calculate_support_resistance(data, window_size):
+    support = data['4a. close (USD)'].rolling(window=window_size).min()
+    resistance = data['4a. close (USD)'].rolling(window=window_size).max()
+    return support, resistance
+
+def analyze_data(data):
+    # Calculate the moving average with a window size of 20
+    data['Moving Average'] = calculate_moving_average(data, 20)
+
+    # Calculate the support and resistance levels with a window size of 20
+    data['Support'], data['Resistance'] = calculate_support_resistance(data, 20)
+
+    return data
