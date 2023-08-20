0df0c4427c94d3becf6d6645f586c0d888dc94ac
Miguel Oliveira <1200874@isep.ipp.pt>
2023-08-20 18:11:15 +0100

    aider: Moved code that fetches and preprocesses data before initializing the TradingEnv in main.py.
    # Aider chat conversation:
    
    USER: Traceback (most recent call last):
      File "c:\Users\mike_\OneDrive\Documentos\StockCryptoAnalyser\main.py", line 11, in <module>
        env = TradingEnv(preprocessed_data)
                         ^^^^^^^^^^^^^^^^^
    NameError: name 'preprocessed_data' is not defined. Did you mean: 'preprocess_data'?
    
    ASSISTANT: The error message indicates that the variable `preprocessed_data` is not defined before it's used to initialize the `TradingEnv` in the `main.py` file.
    
    To fix this, we need to ensure that `preprocessed_data` is defined before it's used. This can be done by moving the code that fetches and preprocesses the data before the `TradingEnv` is initialized.
    
    Here is the *edit block* for the changes:
    
    ```python
    main.py
    <<<<<<< HEAD
    if __name__ == "__main__":
        # Initialize the trading environment and the agent
        env = TradingEnv(preprocessed_data)
        agent = QLearningAgent(env.action_space)
    
        # Train the agent
        num_episodes = 1000
        for episode in range(num_episodes):
            state = env.reset()
            done = False
            while not done:
                action = agent.choose_action(state)
                next_state, reward, done, info = env.step(action)
                agent.learn(state, action, reward, next_state, done)
                state = next_state
    
        # Create the user interface and get the symbol from the user
        symbol = create_interface(agent, env, preprocessed_data)
    =======
    if __name__ == "__main__":
        # Create the user interface and get the symbol from the user
        symbol = create_interface(agent, env, preprocessed_data)
    
        from data_fetching import fetch_data
    
        # Check if the symbol is for a stock or a cryptocurrency
        if symbol in ["AAPL", "MSFT", "GOOGL", "AMZN", "FB"]:  # Add more stock symbols as needed
            # Fetch and preprocess stock data
            data = fetch_data(symbol, "TIME_SERIES_DAILY")
            if data is None:
                print("Error fetching stock data. Exiting program.")
                exit(1)
        else:
            # Fetch and preprocess crypto data
            data = fetch_data(symbol, "DIGITAL_CURRENCY_DAILY")
            if data is None:
                print("Error fetching crypto data. Exiting program.")
                exit(1)
    
        preprocessed_data = preprocess_data(data)
    
        # Initialize the trading environment and the agent
        env = TradingEnv(preprocessed_data)
        agent = QLearningAgent(env.action_space)
    
        # Train the agent
        num_episodes = 1000
        for episode in range(num_episodes):
            state = env.reset()
            done = False
            while not done:
                action = agent.choose_action(state)
                next_state, reward, done, info = env.step(action)
                agent.learn(state, action, reward, next_state, done)
                state = next_state
    >>>>>>> updated
    ```
    This change moves the code that fetches and preprocesses the data before the `TradingEnv` is initialized. This ensures that `preprocessed_data` is defined before it's used.

--- main.py
+++ main.py
@@ -7,6 +7,27 @@ from predictive_analysis import TradingEnv
 from agent import QLearningAgent
 
 if __name__ == "__main__":
+    # Create the user interface and get the symbol from the user
+    symbol = create_interface(agent, env, preprocessed_data)
+
+    from data_fetching import fetch_data
+
+    # Check if the symbol is for a stock or a cryptocurrency
+    if symbol in ["AAPL", "MSFT", "GOOGL", "AMZN", "FB"]:  # Add more stock symbols as needed
+        # Fetch and preprocess stock data
+        data = fetch_data(symbol, "TIME_SERIES_DAILY")
+        if data is None:
+            print("Error fetching stock data. Exiting program.")
+            exit(1)
+    else:
+        # Fetch and preprocess crypto data
+        data = fetch_data(symbol, "DIGITAL_CURRENCY_DAILY")
+        if data is None:
+            print("Error fetching crypto data. Exiting program.")
+            exit(1)
+
+    preprocessed_data = preprocess_data(data)
+
     # Initialize the trading environment and the agent
     env = TradingEnv(preprocessed_data)
     agent = QLearningAgent(env.action_space)
@@ -21,9 +42,6 @@ if __name__ == "__main__":
             next_state, reward, done, info = env.step(action)
             agent.learn(state, action, reward, next_state, done)
             state = next_state
-
-    # Create the user interface and get the symbol from the user
-    symbol = create_interface(agent, env, preprocessed_data)
 
     from data_fetching import fetch_data
 
