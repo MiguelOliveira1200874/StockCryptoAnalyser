f0d1cb8a8717a85caaa0f22161afa1f2d5aad4e0
Miguel Oliveira <1200874@isep.ipp.pt>
2023-08-20 14:51:30 +0100

    Basic interface created and Main error resolved
    Basic interface created and Main error resolved

--- interface.py
+++ interface.py
@@ -1,6 +1,10 @@
 # Code for the user interface
 def create_interface():
     print("Welcome to the Stocks and Crypto Analyzer!")
-    print("Please enter the symbol of the stock or cryptocurrency you want to analyze:")
-    symbol = input()
-    return symbol
+    while True:
+        print("Please enter the symbol of the stock or cryptocurrency you want to analyze:")
+        symbol = input().strip()
+        if symbol:  # basic check to ensure the input is not empty
+            return symbol
+        else:
+            print("Invalid input. Please try again.")
--- main.py
+++ main.py
@@ -8,12 +8,14 @@ if __name__ == "__main__":
     # Create the user interface and get the symbol from the user
     symbol = create_interface()
 
+    from data_fetching import fetch_data
+
     # Fetch and preprocess stock data
-    stock_data = test_application(symbol)
+    stock_data = fetch_data(symbol, "TIME_SERIES_DAILY")
     preprocessed_stock_data = preprocess_data(stock_data)
     visualize_data(preprocessed_stock_data, symbol)
 
     # Fetch and preprocess crypto data
-    crypto_data = test_application(symbol)
+    crypto_data = fetch_data(symbol, "DIGITAL_CURRENCY_DAILY")
     preprocessed_crypto_data = preprocess_data(crypto_data)
     visualize_data(preprocessed_crypto_data, symbol)
